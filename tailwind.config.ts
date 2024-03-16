3e5a62d4fd76eb5d1587a1be1237940107ad070e
Ashley Wu <ashley@lustertech.net>
2024-03-16 16:10:17 +0800

    １Set color palette ２Set font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const colors = require('tailwindcss/colors')
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +9,24 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['var(--font-noto-sans-tc)', 'ui-sans-serif', 'system-ui'],
+      'mono': ['var(--font-noto-sans-mono)', 'ui-monospace', 'SFMono-Regular'],
+    },
+    colors: {
+      primary: 'rgb(var(--color-primary))',
+      primary_light: 'rgb(var(--color-primary) / 10%)',
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: colors.blue,
+      gray: colors.gray,
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
   },
   plugins: [],
