636588c6b912320924e4c7d714fb37c8c4fa5736
Ashley Wu <ashley@lustertech.net>
2024-03-16 21:05:00 +0800

    Fix image problem - 4

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   output: isProd ? 'export' : undefined,
   reactStrictMode: true,
   assetPrefix: isProd ? 'https://ashley-wu.github.io/ProjectOne' : undefined,
-  basePath: isProd ? '/ProjectOne' : undefined,
+  // basePath: isProd ? '/ProjectOne' : undefined,
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
