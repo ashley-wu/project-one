55ec87f8a1f619db35527c62730090f3dc0c43f4
Ashley Wu <ashley@lustertech.net>
2024-03-16 17:11:59 +0800

    Add Line icon link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-'use client'
+import Link from 'next/link'
+
 export default function Home() {
   console.log('NODE_ENV', process.env.NODE_ENV)
   return (
     <main className="min-h-screen grid items-center justify-items-center p-24">
       Project One
+      <div>
+        <Link href='https://line.me/R/ti/p/%40qsj5646s' target='_blank' className='mr-4'>
+          <i className="fa-brands fa-line text-4xl text-gray-100"></i>
+        </Link>
+        <Link href='https://www.google.com' target='_blank'>
+          <i className="fa-brands fa-line text-4xl text-gray-100"></i>
+        </Link>
+      </div>
     </main>
   )
 }
