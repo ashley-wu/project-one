819b68981c98f71411b5ac07ee03c1b4b7dc8125
Ashley Wu <ashley@lustertech.net>
2024-03-16 20:22:18 +0800

    Add image sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
-import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
-  console.log('NODE_ENV', process.env.NODE_ENV)
   return (
     <main className="min-h-screen grid items-center justify-items-center p-24">
       Project One
-      <div>
-        <Link href='https://line.me/R/ti/p/%40qsj5646s' target='_blank' className='mr-4'>
-          <i className="fa-brands fa-line text-4xl text-gray-100"></i>
-        </Link>
-        <Link href='https://www.google.com' target='_blank'>
-          <i className="fa-brands fa-line text-4xl text-gray-100"></i>
-        </Link>
+      <div className='relative h-[400px] w-[400px]'>
+        <Image
+          src={'/image/example.png'}
+          fill
+          alt='example image'
+          style={{ objectFit: 'contain' }}
+        />
       </div>
     </main>
   )
