d4eadcb3bb578ec159c6159e20e819a6edc7fb8a
Ashley Wu <ashley@lustertech.net>
2024-03-17 14:04:58 +0800

    Fix Github Pages config problem v9

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,9 @@ export default function Home() {
     <main className="min-h-screen grid items-center justify-items-center p-24">
       Project One
       <div className='relative h-[400px] w-[400px]'>
+        <img src='./example.png' alt="example image" />
         <Image
-          src={`${pathPrefix}/example.png`}
+          src={`./example.png`}
           // src={'https://lh3.googleusercontent.com/pw/AP1GczMMpuC6OVboI2tCvXP3ENi-Y3HdfcAJahQZCzsWGcAMjy0kD4154mhd9GehJmGQcRx_aINRfk5YPaqRmfA8XnKoCSTkhFTuh4LTRFyD-6tC0lM9SKnSS5D1Zvv5UjgAjtnaNAXZM4H4bAo7Bu9yI0eUDw=w2580-h1452-s-no-gm?authuser=0'}
           fill
           sizes='(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 33vw'
