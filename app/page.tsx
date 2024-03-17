84b20cadadecbfe2b2aadd2df2ad7e9baaef8b2a
Ashley Wu <ashley@lustertech.net>
2024-03-17 11:40:39 +0800

    Fix Github Pages config problem v4

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       Project One
       <div className='relative h-[400px] w-[400px]'>
         <Image
-          src={`/image/example.png`}
+          src={`/example.png`}
           // src={'https://lh3.googleusercontent.com/pw/AP1GczMMpuC6OVboI2tCvXP3ENi-Y3HdfcAJahQZCzsWGcAMjy0kD4154mhd9GehJmGQcRx_aINRfk5YPaqRmfA8XnKoCSTkhFTuh4LTRFyD-6tC0lM9SKnSS5D1Zvv5UjgAjtnaNAXZM4H4bAo7Bu9yI0eUDw=w2580-h1452-s-no-gm?authuser=0'}
           fill
           alt='example image'
